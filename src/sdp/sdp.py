e5ff0bf8c01ae0e854cbaf7434354240079174c1
Paulo <pa.viana@protonmail.ch>
2023-01-18 10:50:13 -0300

    sdp

--- src/sdp/sdp.py
+++ src/sdp/sdp.py
@@ -4,8 +4,16 @@ import networkx as nx
 from src.utils.graph import create_graph
 from src.utils.files import list_files
 import warnings
+import pandas as pd
+import time
 warnings.filterwarnings("ignore")
 
+df = pd.DataFrame()
+run_time = []
+cuts = []
+graph_name = []
+num_edges = []
+num_vertices = []
 
 def cut_size(A, T):
     # Define and solve the CVXPY problem.
@@ -17,7 +25,7 @@ def cut_size(A, T):
     constraints += [cp.diag(X) == 1]
 
     prob = cp.Problem(cp.Minimize(cp.trace(A @ X)), constraints)
-    prob.solve(verbose=False)
+    prob.solve(solver='CVXOPT')
 
     # Solve for the maximum cut
     U = np.linalg.cholesky(X.value)
@@ -35,7 +43,7 @@ def cut_size(A, T):
 if __name__ == '__main__':
 
     np.seterr(all="ignore")
-    path = "../../data/erdos_renyi"
+    path = "data/erdos_renyi"
     files = list_files(path)
     for file in files:
         graph = create_graph(path, file)
@@ -43,7 +51,35 @@ if __name__ == '__main__':
         adj_matrix = nx.adjacency_matrix(graph).toarray()
 
         try:
+            start_cpu_time = time.process_time()
             cut = cut_size(adj_matrix, 100)
-            print(cut)
+            end_cpu_time = time.process_time()
+
+            clock = end_cpu_time - start_cpu_time
+            run_time.append(clock)
+
+            print(f"\nTime = {clock}")
+           
+            cuts.append(cut)
+            print(f"Number of Cuts = {cut}")
+
+            graph_name.append(file)
+            print(f"graph = {file}")
+
+            num_vertices.append(graph.number_of_nodes())
+            print(f"Number of Vertices = {graph.number_of_nodes()}")
+
+            num_edges.append(graph.number_of_edges())
+            print(f"Number of Edges = {graph.number_of_edges()}\n")
+            
+
         except Exception as e:
             print(e)
+
+    df['graph'] = graph_name
+    df['cuts'] = cuts
+    df['vertices'] = num_vertices
+    df['edges'] = num_edges
+    df['time'] = run_time
+
+    df.to_csv("statistics/csv/sdp.csv")
