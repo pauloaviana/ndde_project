fff44a58dc7f5fa34dafc94430017937277fe84d
Icaro Felipe de Lima Ribeiro <iribeiro1@lenovo.com>
2023-01-24 01:50:35 -0300

    Fixing minor bugs

--- src/grid_search.py
+++ src/grid_search.py
@@ -7,28 +7,14 @@ from itertools import product
 import operator
 
 
-df = pd.DataFrame()
-run_time = []
-cuts = []
-best_R = []
-best_L = []
-best_V = []
-gens = []
-graph_name = []
-num_edges = []
-num_vertices = []
-depth = []
-
-
 def evolutionary_process(adj_matrix, adj_list, active_vertices, x, **kwargs):
 
-    prob_size, pop_size, num_gen, mut_par, cross_par = kwargs.values()
     n = len(adj_matrix)
 
     best_list = []
     pop_list = []
 
-    for i in range(1):
+    for i in range(2):
         de = NovelTrevisanDE(vertices_num=n,
                              active_verts=active_vertices,
                              adj_matrix=adj_matrix,
@@ -110,7 +96,7 @@ def grid_search(graph):
                                                best_cross_rates[:1], best_mutation_rates[:1], PROBLEM_SIZES)
 
 
-    index_list = ['prob_size', 'pop_size', 'num_gen', 'mut_par', 'cross_par']
+    index_list = ['pop_size', 'num_gen', 'mut_par', 'cross_par', 'prob_size']
     results_df = pd.DataFrame(index_list)
     best_params = [best_pop_sizes, best_gen_sizes, best_cross_rates, best_mutation_rates, best_prob_sizes]
     for pop_size, num_gen, cross_par, mut_par, prob_size in list(product(*best_params)):
@@ -121,15 +107,22 @@ def grid_search(graph):
             'mut_par': mut_par,
             'cross_par': cross_par
         }
+
+        print(f"Testing parameters: {prob_size} | {num_gen} | {cross_par} | {mut_par} | {cross_par}...")
+
         median_best, median_pop = evolutionary_process(adj_matrix, adj_list, active_vertices, x, **kwargs)
-        for i in range(median_best):
+
+        size = len(median_best) - 1
+        print(f"...Results = Best median fitness: {median_best[size]} | Population Median fitness: {median_pop[size]}")
+
+        for i in range(len(median_best)):
             kwargs[str(i)] = median_best[i]
 
         new_df = pd.DataFrame(kwargs, index=[1])
         new_df = new_df.set_index(index_list)
         results_df.append(new_df)
 
-    results_df.to_csv("./statistics/csv_files/grid_search_icaro_V2.csv")
+    results_df.to_csv("../statistics/csv_files/grid_search_icaro_V2.csv")
 
 
 
